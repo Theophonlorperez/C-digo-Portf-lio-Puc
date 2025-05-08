2357ed7fba3578b33ddde516c1a23611849d0206
Theo Perez <theoperez112006@gmail.com>
2025-05-08 14:37:09 -0300

    Update map.js
    implementando map

---
+++ 01/map.js
@@ -0,0 +1,15 @@
+// TDD - teste driven development
+var novoMapa = new Map();
+
+class historyMap{
+   
+}
+
+// 11/11/2011 as 11 da noite 
+novoMapa.set('11/11/11 - 23:30 - URL',{title:'titulo do site', url:'url sozinha'});
+novoMapa.set('11/11/11 - 23:30 - URL',{});
+novoMapa.set('11/11/11 - 23:30 - URL',{});
+novoMapa.set('11/11/11 - 23:00 - URL',{});
+
+var resultado = novoMapa;
+console.log(resultado); 
